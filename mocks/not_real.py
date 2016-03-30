3771ff31374f983e75ae56cea771a0fae1a8bd31
fathergoose <al@ilseman.com>
2016-03-30 15:31:22 -0500

    added git ignore and get_customer_scoring()

---
+++ mocks/not_real.py
@@ -0,0 +1,13 @@
+from httmock import response, urlmatch
+
+NETLOC = r'(.*\.)?not_real\.com$'
+HEADERS = {'content-type': 'application/json'}
+
+@urlmatch(
+        netloc=NETLOC,
+        path='/customer_scoring',
+        method='get',
+        query=r'^income=\d+\&zipcode=\d+\&age=\d'
+        )
+def customer_scoring_query_mock(self, url, request):
+    return {
---
+++ not_real.py
@@ -0,0 +1,12 @@
+import requests
+
+BASE_DOMAIN = 'not_real.com'
+
+def get_customer_scoring(income, zipcode, age):
+    uri = 'http://{0}/customer_scoring?income={1}&zipcode={2}&age={3}'\
+    .format(BASE_DOMAIN, income, zipcode, age)
+    print(uri)
+    # response = requests.get(uri)
+    # response.raise_for_status()
+    # return response.json()
+
