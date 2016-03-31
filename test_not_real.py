d292710670e28f4f6fef30f99ce38460b7467a0e
fathergoose <al@ilseman.com>
2016-03-31 18:06:32 -0500

    wrote tests for one and two argument get_customer_scoring()

--- test_not_real.py
+++ test_not_real.py
@@ -8,18 +8,44 @@ import mocks.not_real
 class TestNotReal(unittest.TestCase):
 
     @with_httmock(mocks.not_real.customer_scoring_query_mock)
-    def test_get_customer_scoring(self):
+    def test_get_customer_scoring_with_one_arguments(self):
+        income = 60000
+
+        results_w_one = not_real.get_customer_scoring(income)
+
+        self.assertNotEqual(results_w_one, None)
+        self.assertIsInstance(results_w_one, dict)
+        self.assertTrue('propensity' in results_w_one)
+        self.assertTrue('ranking' in results_w_one)
+        self.assertEqual(results_w_one['ranking'], 'C')
+
+    @with_httmock(mocks.not_real.customer_scoring_query_mock)
+    def test_get_customer_scoring_with_two_arguments(self):
+        income = 60000
+        zipcode = 60626
+
+        results_w_two = not_real.get_customer_scoring(income, zipcode)
+
+        self.assertNotEqual(results_w_two, None)
+        self.assertIsInstance(results_w_two, dict)
+        self.assertTrue('propensity' in results_w_two)
+        self.assertTrue('ranking' in results_w_two)
+        self.assertEqual(results_w_two['ranking'], 'C')
+
+    @with_httmock(mocks.not_real.customer_scoring_query_mock)
+    def test_get_customer_scoring_with_three_arguments(self):
         income = 60000
         zipcode = 60626
         age = 27
 
-        results = not_real.get_customer_scoring(income, zipcode, age)
+        results_w_three = not_real.get_customer_scoring(income, zipcode, age)
 
-        self.assertNotEqual(results, None)
-        self.assertIsInstance(results, dict)
-        self.assertTrue('propensity' in results)
-        self.assertTrue('ranking' in results)
-        self.assertEqual(results['ranking'], 'C')
+        self.assertNotEqual(results_w_three, None)
+        self.assertIsInstance(results_w_three, dict)
+        self.assertTrue('propensity' in results_w_three)
+        self.assertTrue('ranking' in results_w_three)
+        self.assertEqual(results_w_three['ranking'], 'C')
+
 
     def test_get_customer_scroing_without_arguments(self):
         results = not_real.get_customer_scoring()
