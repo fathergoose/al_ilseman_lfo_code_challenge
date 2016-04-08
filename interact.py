e34504bd9f65f3e451c78469f1f6ce3e815a4fd2
fathergoose <al@ilseman.com>
2016-04-07 21:09:54 -0500

    added looping and server response

--- interact.py
+++ interact.py
@@ -1,25 +1,37 @@
 import not_real
 
 def main():
-    customer_info = dict()
+    running = True
+    while running:
+        customer_info = dict()
 
-    print "Get a customer's scoring"
-    print "Leave blank if unknown"
+        print "Get a customer's scoring"
+        print "Leave blank if unknown"
 
-    raw_income = raw_input("Enter the customer's anual income in USD: ")
-    if raw_income != '':
-        customer_info['income'] = raw_income 
+        raw_income = raw_input("Enter the customer's anual income in USD: ")
+        if raw_income != '':
+            customer_info['income'] = raw_income 
 
-    raw_zipcode = raw_input("And their zipcode: ")
-    if raw_zipcode != '':
-        customer_info['zipcode'] = raw_zipcode
+        raw_zipcode = raw_input("And their zipcode: ")
+        if raw_zipcode != '':
+            customer_info['zipcode'] = raw_zipcode
 
-    raw_age = raw_input("Finaly their age: ")
-    if raw_age != '':
-        customer_info['age'] = raw_age
+        raw_age = raw_input("Finaly their age: ")
+        if raw_age != '':
+            customer_info['age'] = raw_age
 
-    not_real.get_customer_scoring(customer_info)
+        customer_scoring = not_real.get_customer_scoring(customer_info)
+        
+        print(customer_scoring)
 
-    raw_input('Hold on')
+        go_again = raw_input('Continue? (Y/N) ').lower()
+
+        if 'y' in go_again:
+            running = True
+        else:
+            running = False
+
+        
+
 
 main()
