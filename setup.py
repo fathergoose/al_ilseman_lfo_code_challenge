0202b36be667f1aea206a0ef54c2aa33ce0c49b1
fathergoose <al@ilseman.com>
2016-04-07 22:44:18 -0500

    packaging info

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='not_real',
+      version='0.2.1',
+      py_modules=['not_real'],
+      author='al ilseman',
+      author_email='al@ilseman.com',
+      install_requires=[
+          'requests'
+      ]
+      ) 
